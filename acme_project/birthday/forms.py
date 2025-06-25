50cd05031ae9bd3bcd36794a32e39169b1c9a75e
Wallery <Wallery@mail.ru>
2025-06-26 00:30:17 +0300

    25.06.2025 - подсчет дней до ДР. Модель формы.

--- acme_project/birthday/forms.py
+++ acme_project/birthday/forms.py
@@ -1,11 +1,20 @@
 # birthday/forms.py
 from django import forms
 
+# Импортируем класс модели Birthday.
+from .models import Birthday
 
-class BirthdayForm(forms.Form):
-    first_name = forms.CharField(label='Имя', max_length=20)
-    last_name = forms.CharField(label='Фамилия', required=False,
-                                help_text='необязательное поле')
-    birthday = forms.DateField(label='Дата рождения',
-        # Указываем, что виджет для ввода даты должен быть с типом date.
-        widget=forms.DateInput(attrs={'type': 'date'}))
+
+# Для использования формы с моделями меняем класс на forms.ModelForm.
+class BirthdayForm(forms.ModelForm):
+    # Удаляем все описания полей.
+
+    # Все настройки задаём в подклассе Meta.
+    class Meta:
+        # Указываем модель, на основе которой должна строиться форма.
+        model = Birthday
+        # Указываем, что надо отобразить все поля.
+        fields = '__all__'
+        widgets = {
+            'birthday': forms.DateInput(attrs={'type': 'date'})
+        }
