5cd0a3551e9257bb673b02fc34dc55fff3ace078
Wallery <Wallery@mail.ru>
2025-06-26 19:29:00 +0300

    25.06.2025 - подсчет дней до ДР. Запись в БД

--- acme_project/birthday/views.py
+++ acme_project/birthday/views.py
@@ -2,11 +2,12 @@ from django.shortcuts import render
 
 from .forms import BirthdayForm
 from .utils import calculate_birthday_countdown
+from .models import Birthday
 
 def birthday(request):
-    form = BirthdayForm(request.GET or None)
+    form = BirthdayForm(request.POST or None)
     if form.is_valid():
-        pass
+        form.save()
     context = {'form': form}
     if form.is_valid():
         # ...вызовем функцию подсчёта дней:
@@ -17,3 +18,11 @@ def birthday(request):
         # Обновляем словарь контекста: добавляем в него новый элемент.
         context.update({'birthday_countdown': birthday_countdown})
     return render(request, 'birthday/birthday.html', context)
+
+
+def birthday_list(request):
+    # Получаем все объекты модели Birthday из БД.
+    birthdays = Birthday.objects.all()
+    # Передаём их в контекст шаблона.
+    context = {'birthdays': birthdays}
+    return render(request, 'birthday/birthday_list.html', context)
